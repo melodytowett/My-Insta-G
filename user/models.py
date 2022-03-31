50977edca5811818b93fbf36e1cc2ce526b5630d
melodytowett <melodytowett99@gmail.com>
2022-04-01 01:16:33 +0300

    add models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
+from distutils.command.upload import upload
+from operator import mod
+import profile
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = '/article',blank =True)
+    name = models.CharField(max_length=40)
+    caption = models.CharField(max_length=50)
+    profile = models.ForeignKey()
+    likes = models.CharField()
+    comments = models
