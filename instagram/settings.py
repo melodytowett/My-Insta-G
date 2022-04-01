b58223f464ce3d6fbfc94b552449a05841f59b90
melodytowett <melodytowett99@gmail.com>
2022-04-01 12:26:45 +0300

    create super user

--- instagram/settings.py
+++ instagram/settings.py
@@ -19,7 +19,7 @@ from decouple import config,Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY = config('SECRET_KEY')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
--- user/models.py
+++ user/models.py
@@ -6,14 +6,19 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to='',blank=True)
-    bio = models.CharField
+    profile_photo = CloudinaryField('image',blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.profile_photo
 
 class Image(models.Model):
-    image = CloudinaryField ('image')
     image_name = models.CharField(max_length=40)
+    image = CloudinaryField ('image')
     image_caption = models.CharField(max_length=200)
     profile = models.ForeignKey(Profile ,on_delete=models.CASCADE)
     likes = models.IntegerField(default=0)
-    comments = models
+    comments = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.image_name
