02f9b7ecc4b7646140e296447f03d73cbe7f855b
melodytowett <melodytowett99@gmail.com>
2022-04-01 15:50:26 +0300

    test the image and profile models

--- user/models.py
+++ user/models.py
@@ -17,9 +17,9 @@ class Profile(models.Model):
 
 class Image(models.Model):
     image_name = models.CharField(max_length=40)
-    image = CloudinaryField ('image')
+    image = CloudinaryField ('image',blank=True)
     image_caption = models.CharField(max_length=200)
-    profile = models.ForeignKey(Profile ,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile ,on_delete=models.CASCADE,null=True)
     likes = models.IntegerField(default=0)
     comments = models.CharField(max_length=100)
 
@@ -29,7 +29,7 @@ class Image(models.Model):
     def save_image(self):
         self.save()
 
-
+    
     @classmethod
     def my_page(cls,name):
         page = cls.objects.filter(image_name__in=name)
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
 
+from django.test import TestCase
+from .models import Image,Profile
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.images = Image(image_name="Beach",image_caption="Beach in africa",likes=0,comments="amazing")
+        self.images.save_image()
+
+    def test_image_instance(self):
+        self.assertTrue(isinstance(self.images,Image))
+
+    def test_save_page(self):
+        self.images.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.profile = Profile(bio = "This is what you dont know about me")
+
+    def test_profile_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_save_profile(self):
+        self.profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        
