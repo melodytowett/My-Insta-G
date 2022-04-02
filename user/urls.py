023bebd9146b0af61d78e4bf5c728c6efcb107ea
melodytowett <melodytowett99@gmail.com>
2022-04-02 13:00:36 +0300

    login user

--- user/urls.py
+++ user/urls.py
@@ -9,6 +9,7 @@ from . import views
 urlpatterns = [
     path('',views.my_page,name='my_page'),
     path('register',views.register_user,name='register'),
+    path('login',views.login_user,name="login")
     # path('accounts/', include('django_registration.backends.one_step.urls')),
     # path('accounts/', include('django.contrib.auth.urls')),
 ]
--- user/views.py
+++ user/views.py
@@ -1,11 +1,13 @@
 
+from django.http import request
 from django.shortcuts import render,redirect
 
 import user
 from .models import Image
 from .forms import NewUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login,authenticate
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 def my_page(request):
@@ -25,4 +27,23 @@ def register_user(request):
         messages.error(request,"Invalid credentials.")
     form=NewUserForm()
     return render(request,'registration/register.html',context={"register_form":form})
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                # messages.info(request,)
+                return redirect("user:my_page")
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"invalid username or password.")
+    form = AuthenticationForm()
+    return render(request,'registration/login.html',context={"login_form":form})
+
 
